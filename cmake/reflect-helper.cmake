51d43fad0ca7b7b61ad7a182a40d3bb79d6d7565
lightning <xuyuanwang0000@gmail.com>
2024-12-06 14:35:48 +0800

    add reflect helper

---
+++ cmake/reflect-helper.cmake
@@ -0,0 +1,19 @@
+
+function(fetch_reflection_src GroupName DstSrcs)
+set(__REFLECTIONT_PROPERTY_NAME__ "__Reflect_group_${GroupName}__")
+get_property(srcs GLOBAL PROPERTY ${__REFLECTIONT_PROPERTY_NAME__})
+list(APPEND ${DstSrcs} ${srcs})
+set(${DstSrcs} ${${DstSrcs}} PARENT_SCOPE)
+endfunction()
+
+
+function(register_reflection_src GroupName RegisterSrcs)
+fetch_reflection_src(${GroupName} __CURRENT_VALUE__)
+list(APPEND __CURRENT_VALUE__ ${RegisterSrcs})
+set(__REFLECTIONT_PROPERTY_NAME__ "__Reflect_group_${GroupName}__")
+set_property(GLOBAL PROPERTY ${__REFLECTIONT_PROPERTY_NAME__} ${__CURRENT_VALUE__})
+endfunction()
+
+
+
+
