c4f734bd3576cfdece0b8278a0a56a69a93a8dc2
lightning <469953258@qq.com>
2025-01-08 14:30:44 +0800

    add object allocator pool

--- samples/TestMemoryPool/src/TestMemoryPool.cpp
+++ samples/TestMemoryPool/src/TestMemoryPool.cpp
@@ -1,38 +1,31 @@
+#define AllocatorTracker 1
+
 #include "fundamental/basic/allocator.hpp"
+#include "fundamental/basic/log.h"
 
 #include <array>
+#include <deque>
 #include <iostream>
 #include <memory_resource>
 #include <vector>
-#include <deque>
 
-
-class tracer_memory_resource : public std::pmr::memory_resource
-{
+class tracer_memory_resource : public std::pmr::memory_resource {
 public:
-    explicit tracer_memory_resource(std::pmr::memory_resource* refsource) :
-    ref(refsource)
-    {
+    explicit tracer_memory_resource(std::pmr::memory_resource* refsource) : ref(refsource) {
     }
 
 protected:
-    void*
-    do_allocate(size_t __bytes, size_t __alignment) override
-    {
+    void* do_allocate(size_t __bytes, size_t __alignment) override {
         std::cout << "allocate " << __bytes << std::endl;
         return ref->allocate(__bytes, __alignment);
     }
 
-    void
-    do_deallocate(void* __p, size_t __bytes, size_t __alignment) override
-    {
+    void do_deallocate(void* __p, size_t __bytes, size_t __alignment) override {
         std::cout << "deallocate " << __bytes << std::endl;
         ref->deallocate(__p, __bytes, __alignment);
     }
 
-    bool
-    do_is_equal(const memory_resource& __other) const noexcept override
-    {
+    bool do_is_equal(const memory_resource& __other) const noexcept override {
         return this == &__other;
     }
 
@@ -40,54 +33,102 @@ private:
     std::pmr::memory_resource* const ref;
 };
 
-int main()
-{
-    std::pmr::memory_resource* upstream = std::pmr::new_delete_resource();
-    tracer_memory_resource source(upstream);
-    std::pmr::pool_options options;
-    constexpr std::size_t kLargeBlockSize = 4098;
-    options.largest_required_pool_block   = kLargeBlockSize;
-    options.max_blocks_per_chunk          = 4;
-    
-    auto poolResource= Fundamental::MakePoolMemorySource(options, &source);
+void TestObjectPool();
 
-    std::pmr::memory_resource* activeSource = poolResource.get();
+int main() {
     {
-        std::cout << "test int vec " << std::endl;
-        std::pmr::vector<int> myVector { activeSource };
+        std::pmr::memory_resource* upstream = std::pmr::new_delete_resource();
+        tracer_memory_resource source(upstream);
+        std::pmr::pool_options options;
+        constexpr std::size_t kLargeBlockSize = 4098;
+        options.largest_required_pool_block   = kLargeBlockSize;
+        options.max_blocks_per_chunk          = 4;
 
-        for (int i = 0; i < 32; ++i)
+        auto poolResource = Fundamental::MakePoolMemorySource(options, &source);
+
+        std::pmr::memory_resource* activeSource = poolResource.get();
         {
-            std::cout<<"emplace"<<std::endl;
-            myVector.push_back(i);
+            std::cout << "test int vec " << std::endl;
+            std::pmr::vector<int> myVector { activeSource };
+
+            for (int i = 0; i < 32; ++i) {
+                std::cout << "emplace" << std::endl;
+                myVector.push_back(i);
+            }
+            std::cout << "clear" << std::endl;
+            myVector.clear();
+            std::cout << "shrink_to_fit" << std::endl;
+            myVector.shrink_to_fit();
         }
-        std::cout << "clear" << std::endl;
-        myVector.clear();
-        std::cout << "shrink_to_fit" << std::endl;
-        myVector.shrink_to_fit();
+
+        struct LargeBlock {
+            char data[kLargeBlockSize * 2 + 1];
+        };
+        {
+            std::cout << "test large block vec " << sizeof(LargeBlock) << std::endl;
+            std::pmr::vector<LargeBlock> testVec { activeSource };
+            for (int i = 0; i < 10; ++i) {
+                std::cout << "emplace" << std::endl;
+                testVec.emplace_back();
+            }
+
+            std::cout << "clear" << std::endl;
+            testVec.clear();
+            std::cout << "shrink_to_fit" << std::endl;
+            testVec.shrink_to_fit();
+        }
+        { std::deque<int, std::pmr::polymorphic_allocator<int>> dq; }
+        std::cout << "finished" << std::endl;
     }
 
-    struct LargeBlock
-    {
-        char data[kLargeBlockSize * 2+1];
+    TestObjectPool();
+    return 0;
+}
+struct TestMem {
+    inline static uint8_t s_loop_value;
+    uint8_t bytes[32];
+    std::uint64_t k;
+    TestMem() {
+        bytes[0] = s_loop_value++;
+        memset(bytes + 1, '1', 31);
+        FDEBUG("TestMem constructor {}", static_cast<int>(bytes[0]));
     };
-    {
-        std::cout << "test large block vec " << sizeof(LargeBlock) << std::endl;
-        std::pmr::vector<LargeBlock> testVec { activeSource };
-        for (int i = 0; i < 10; ++i)
-        {
-            std::cout<<"emplace"<<std::endl;
-            testVec.emplace_back();
+    ~TestMem() {
+        FDEBUG("TestMem destructor {}", static_cast<int>(bytes[0]));
+    }
+};
+void TestObjectPool() {
+    using SafePool = Fundamental::ThreadSafeObjectPoolAllocator<TestMem>;
+    static_assert(std::is_same_v<SafePool::PoolSourceType_, std::pmr::synchronized_pool_resource>);
+    using UnsafePool = Fundamental::ThreadUnSafeObjectPoolAllocator<TestMem>;
+    static_assert(std::is_same_v<UnsafePool::PoolSourceType_, std::pmr::unsynchronized_pool_resource>);
+
+    SafePool pool;
+    for (int i = 0; i < 10; ++i) {
+        try {
+            FDEBUG("allocate {}", i);
+            auto* p = pool.allocate(1);
+            FDEBUG("construct {}", i);
+            pool.construct(p);
+            FDEBUG("destroy {}", i);
+            pool.destroy(p);
+            FDEBUG("deallocate {}", i);
+            pool.deallocate(p, 1);
+        } catch (const std::exception& e) {
+            std::cerr << e.what() << '\n';
         }
-            
-        std::cout << "clear" << std::endl;
-        testVec.clear();
-        std::cout << "shrink_to_fit" << std::endl;
+    }
+    {
+        std::vector<TestMem, SafePool> testVec;
+        FDEBUG("resize 10");
+        testVec.resize(10);
+        FDEBUG("shrink_to_fit");
         testVec.shrink_to_fit();
     }
     {
-        std::deque<int,std::pmr::polymorphic_allocator<int>> dq;
+        FDEBUG("new object");
+        auto testobject = pool.NewObject();
+        FDEBUG("delete object");
+        pool.DeleteObject(testobject);
     }
-    std::cout << "finished" << std::endl;
-    return 0;
 }
--- src/fundamental/basic/allocator.hpp
+++ src/fundamental/basic/allocator.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <memory>
 #include <memory_resource>
+#ifdef AllocatorTracker
+    #include <iostream>
+#endif
 namespace Fundamental {
 template <typename _Tp>
 using AllocatorType = std::pmr::polymorphic_allocator<_Tp>;
@@ -24,5 +27,62 @@ template <typename... Args>
 decltype(auto) MakeMonoBufferMemorySource(Args&&... args) {
     return MakeSharedMemorySource<std::pmr::monotonic_buffer_resource>(std::forward<Args>(args)...);
 }
+namespace internal {
+template <size_t ObjectSize>
+inline constexpr decltype(auto) GetPoolOption() {
+    return std::pmr::pool_options { 4, ObjectSize * 4 };
+}
+} // namespace internal
 
+// fixed-size object allocator
+template <typename ObjectType, bool ThreadSafe,
+          typename PoolSourceType = std::conditional_t<ThreadSafe, std::pmr::synchronized_pool_resource,
+                                                       std::pmr::unsynchronized_pool_resource>>
+struct ObjectPoolAllocator : std::pmr::polymorphic_allocator<ObjectType> {
+    using PoolSourceType_ = PoolSourceType;
+    std::shared_ptr<std::pmr::memory_resource> source;
+    ObjectPoolAllocator() :
+    std::pmr::polymorphic_allocator<ObjectType>(new PoolSourceType(internal::GetPoolOption<sizeof(ObjectType)>())),
+    source(std::shared_ptr<std::pmr::memory_resource>(std::pmr::polymorphic_allocator<ObjectType>::resource())) {
+    }
+#ifdef AllocatorTracker
+    [[nodiscard]]
+    ObjectType* allocate(size_t __n) __attribute__((__returns_nonnull__)) {
+        if ((__gnu_cxx::__int_traits<size_t>::__max / sizeof(ObjectType)) < __n) std::__throw_bad_array_new_length();
+        std::cout << "allocate align_size:" << alignof(ObjectType) << " total:" << __n * sizeof(ObjectType)
+                  << " count:" << __n << std::endl;
+        return static_cast<ObjectType*>(source->allocate(__n * sizeof(ObjectType), alignof(ObjectType)));
+    }
+
+    void deallocate(ObjectType* __p, size_t __n) noexcept __attribute__((__nonnull__)) {
+        std::cout << "deallocate align_size:" << alignof(ObjectType) << " total:" << __n * sizeof(ObjectType)
+                  << " count:" << __n << std::endl;
+        source->deallocate(__p, __n * sizeof(ObjectType), alignof(ObjectType));
+    }
+#endif
+
+    template <typename... _CtorArgs>
+    [[nodiscard]] ObjectType* NewObject(_CtorArgs&&... __ctor_args) {
+        auto* __p = std::pmr::polymorphic_allocator<ObjectType>::allocate(1);
+        __try {
+            std::pmr::polymorphic_allocator<ObjectType>::construct(__p, std::forward<_CtorArgs>(__ctor_args)...);
+        }
+        __catch(...) {
+            std::pmr::polymorphic_allocator<ObjectType>::deallocate(__p, 1);
+            __throw_exception_again;
+        }
+        return __p;
+    }
+
+    void DeleteObject(ObjectType* __p) {
+        std::pmr::polymorphic_allocator<ObjectType>::destroy(__p);
+        std::pmr::polymorphic_allocator<ObjectType>::deallocate(__p, 1);
+    }
+};
+
+template <typename ObjectType>
+struct ThreadSafeObjectPoolAllocator : ObjectPoolAllocator<ObjectType, true> {};
+
+template <typename ObjectType>
+struct ThreadUnSafeObjectPoolAllocator : ObjectPoolAllocator<ObjectType, false> {};
 } // namespace Fundamental
