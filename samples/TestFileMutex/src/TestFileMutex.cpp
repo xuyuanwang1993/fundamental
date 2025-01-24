d08acf28fa709f73f8eea21a132e6dd567bf49be
lightning <469953258@qq.com>
2025-01-24 17:27:58 +0800

    tracker all files

---
+++ samples/TestFileMutex/src/TestFileMutex.cpp
@@ -0,0 +1,22 @@
+#include "fundamental/basic/log.h"
+#include "fundamental/basic/mutext_utils.hpp"
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        printf("%s <lock_name>\n", argv[0]);
+        return 1;
+    }
+    Fundamental::file_mutex mutex(argv[1]);
+    auto ret = mutex.Lock();
+    if (!ret) {
+        FERR("Failed to lock file:{}", argv[1]);
+        return 1;
+    } else {
+        FINFO("lock {} success", argv[1]);
+    }
+    FASSERT(mutex.IsLocked());
+    getchar();
+    mutex.Unlock();
+    FINFO("unlock {} success", argv[1]);
+
+    return 0;
+}
---
+++ src/fundamental/basic/mutext_utils.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include "fundamental/basic/base64_utils.hpp"
+
+#include <fcntl.h>
+#include <sys/file.h>
+#include <unistd.h>
+
+#include <cassert>
+#include <cstring>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+namespace Fundamental {
+
+class file_mutex {
+    static constexpr const char* kLckFileExt = ".lck";
+    static constexpr const char* kLckFileDir = "/tmp/";
+
+public:
+    file_mutex(std::string_view lock_name) {
+        assert(lock_name.size() > 0);
+        _lock_filename = std::string(kLckFileDir) +
+                         Fundamental::Base64Encode<Base64CoderType::kFSBase64>(lock_name.data(), lock_name.size()) +
+                         kLckFileExt;
+    }
+
+    ~file_mutex() {
+        Unlock();
+    }
+
+    bool Lock() {
+        fd = open(_lock_filename.c_str(), O_RDWR | O_CREAT | __O_CLOEXEC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+        if (fd < 0) {
+            return false;
+        }
+        auto ret = flock(fd, LOCK_EX | LOCK_NB);
+        if (ret != 0) {
+            close(fd);
+            fd = -1;
+            return false;
+        }
+        return true;
+    }
+
+    bool Unlock() {
+        if (fd >= 0) {
+            // Release the lock and close the file descriptor
+            flock(fd, LOCK_UN);
+            close(fd);
+            fd = -1;
+            // Remove the lock file
+            std::remove(_lock_filename.c_str());
+        }
+        return true;
+    }
+
+    bool IsLocked() const {
+        return fd >= 0;
+    }
+
+private:
+    std::string _lock_filename;
+    std::int32_t fd;
+};
+
+}; // namespace Fundamental
