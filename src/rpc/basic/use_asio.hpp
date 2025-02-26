f4cfe07d65a43f630e75d079c0889968498d4352
lightning <469953258@qq.com>
2025-02-26 17:12:17 +0800

    fix wolf ssl error

--- src/rpc/basic/use_asio.hpp
+++ src/rpc/basic/use_asio.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
+#ifndef ASIO_USE_WOLFSSL
+    #undef SHA256
+#endif
+
 #include <asio.hpp>
 #include <asio/detail/noncopyable.hpp>
+#include <asio/ssl.hpp>
 #include <asio/steady_timer.hpp>
 #include <string_view>
-#include <asio/ssl.hpp>
 
 namespace network {
-using tcp_socket = asio::ip::tcp::socket;
+using tcp_socket  = asio::ip::tcp::socket;
 using string_view = std::string_view;
-}
-
+} // namespace network
