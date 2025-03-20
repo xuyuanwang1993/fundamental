2943491993457b01149e581ac78bb51e8ab8ba8a
lightning <469953258@qq.com>
2025-03-20 19:49:11 +0800

    data storage add any type value suppport

--- src/fundamental/data_storage/data_storage_interface.hpp
+++ src/fundamental/data_storage/data_storage_interface.hpp
@@ -9,51 +9,57 @@ namespace Fundamental
 {
 using ExpiredSignalType =
     Fundamental::Signal<Fundamental::SignalBrokenType(std::string_view /*table_name*/, std::string_view /*key*/)>;
-template <class derived>
-class data_storage_interface{
-public:
-    using super = derived;
+
+template <class derived_t>
+class data_storage_interface {
 
 public:
     bool init() {
-        return imp().init();
-    }
-
-    // return true if persist data success
-    bool persist_data(std::string table_name, std::string key, std::string data, const storage_config& config) {
-        return imp().persist_data(std::move(table_name), std::move(key), std::move(data), config);
+        return derived_t::init();
     }
 
     std::size_t table_size(std::string table_name) const {
-        return imp().table_size(std::move(table_name));
+        return derived_t::table_size(std::move(table_name));
     }
 
     bool has_key(std::string table_name, std::string key) const {
-        return imp().has_key(std::move(table_name), std::move(key));
+        return derived_t::has_key(std::move(table_name), std::move(key));
     }
 
     bool remove_data(std::string table_name, std::string key) {
-        return imp().remove_data(std::move(table_name), std::move(key));
+        return derived_t::remove_data(std::move(table_name), std::move(key));
     }
 
     bool update_key_expired_time(std::string table_name, std::string key, std::int64_t update_expired_time_msec) {
-        return imp().update_key_expired_time(std::move(table_name), std::move(key), update_expired_time_msec);
-    }
-
-    std::tuple<bool, std::string> get_value(std::string table_name, std::string key) const {
-        return imp().get_value(std::move(table_name), std::move(key));
+        return derived_t::update_key_expired_time(std::move(table_name), std::move(key), update_expired_time_msec);
     }
 
     ExpiredSignalType& expired_signal() {
-        return imp().expired_signal();
+        return derived_t::expired_signal();
+    }
+};
+
+template <typename value_type, class derived_t>
+class data_storage_acessor {
+public:
+    // return true if persist data success
+    bool persist_data(std::string table_name, std::string key, value_type data, const storage_config& config) {
+        return derived_t::persist_data(std::move(table_name), std::move(key), std::move(data), config);
     }
 
-protected:
-    derived& imp() {
-        return *(static_cast<derived*>(this));
-    };
-    const derived& imp() const {
-        return *(static_cast<const derived*>(this));
-    };
+    std::tuple<bool, value_type> get_value(std::string table_name, std::string key) const {
+        return derived_t::get_value(std::move(table_name), std::move(key));
+    }
+};
+
+template <class derived_t>
+class data_storage_acessor<void, derived_t> {
+public:
+    using value_type = void;
+
+public:
+    bool persist_data(std::string table_name, std::string key, const storage_config& config) {
+        return derived_t::persist_data(std::move(table_name), std::move(key), config);
+    }
 };
 } // namespace Fundamental
--- src/fundamental/data_storage/memory_data_storage.hpp
+++ src/fundamental/data_storage/memory_data_storage.hpp
@@ -1,36 +1,194 @@
 #pragma once
 #include "data_storage_interface.hpp"
 
+#include "fundamental/basic/log.h"
+#include "fundamental/delay_queue/delay_queue.h"
+
+#include <unordered_map>
+
 namespace Fundamental
 {
-class DelayQueue;
-class memory_storage : public data_storage_interface<memory_storage>{
-    struct PrivateData;
+
+namespace storage
+{
+template <typename DataType, typename = std::void_t<>>
+struct memory_storage_item {
+    DataType data;
+    Fundamental::DelayQueue::HandleType handle = Fundamental::DelayQueue::kInvalidHandle;
+};
+
+template <typename DataType>
+struct memory_storage_item<DataType, std::void_t<std::enable_if_t<std::is_void_v<DataType>>>> {
+    Fundamental::DelayQueue::HandleType handle = Fundamental::DelayQueue::kInvalidHandle;
+};
+
+template <typename ValueType>
+class memory_storage_data : public data_storage_interface<memory_storage_data<ValueType>> {
+protected:
+    using storage_table_type  = std::unordered_map<std::string, storage::memory_storage_item<ValueType>>;
+    using storage_tables_type = std::unordered_map<std::string, storage_table_type>;
+
 public:
-    memory_storage(DelayQueue * delay_queue);
-    ~memory_storage();
-    bool init();
+    using value_type = ValueType;
 
-    // return true if persist data success
-    bool persist_data(std::string table_name,
-                      std::string key,
-                      std::string data,
-                      const storage_config& config);
+public:
+    memory_storage_data(DelayQueue* delay_queue) : delay_queue(delay_queue) {
+        FASSERT_ACTION(delay_queue != nullptr, throw std::runtime_error("need a valid delay queue ref"),
+                       "need a valid delay queu ref");
+    }
+    ~memory_storage_data() {
+    }
+    bool init() {
+        return true;
+    }
 
-    std::size_t table_size(std::string table_name) const;
+    std::size_t table_size(std::string table_name) const {
+        std::scoped_lock<std::mutex> locker(data_mutex);
+        auto table_iter = storage.find(table_name);
+        return table_iter == storage.end() ? 0 : table_iter->second.size();
+    }
 
-    bool has_key(std::string table_name, std::string key) const;
+    bool has_key(std::string table_name, std::string key) const {
+        std::scoped_lock<std::mutex> locker(data_mutex);
+        auto table_iter = storage.find(table_name);
+        return table_iter == storage.end() ? false : table_iter->second.count(key) > 0;
+    }
 
-    bool remove_data(std::string table_name, std::string key);
-    bool update_key_expired_time(std::string table_name,
-                                 std::string key,
-                                 std::int64_t update_expired_time_msec);
+    bool remove_data(std::string table_name, std::string key) {
+        std::scoped_lock<std::mutex> locker(data_mutex);
+        auto table_iter = storage.find(table_name);
+        return table_iter == storage.end() ? false : table_iter->second.erase(key) > 0;
+    }
+    bool update_key_expired_time(std::string table_name, std::string key, std::int64_t update_expired_time_msec) {
+        std::scoped_lock<std::mutex> locker(data_mutex);
+        auto table_iter = storage.find(table_name);
+        if (table_iter == storage.end()) return false;
+        auto data_iter = table_iter->second.find(key);
+        return data_iter == table_iter->second.end()
+                   ? false
+                   : delay_queue->ModifyTaskNextExpiredTimepoint(data_iter->second.handle, update_expired_time_msec);
+    }
 
-    std::tuple<bool, std::string> get_value(std::string table_name, std::string key) const;
+    ExpiredSignalType& expired_signal() {
+        return expired_signal_;
+    }
 
-    ExpiredSignalType& expired_signal();
+protected:
+    void release_data() {
+        std::scoped_lock<std::mutex> locker(data_mutex);
+        for (auto& table : storage) {
+            for (auto& item : table.second) {
+                delay_queue->StopDelayTask(item.second.handle);
+            }
+        }
+    }
 
-private:
-    PrivateData* p_data;
+protected:
+    ExpiredSignalType expired_signal_;
+    mutable std::mutex data_mutex;
+    DelayQueue* const delay_queue;
+    storage_tables_type storage;
+};
+
+} // namespace storage
+
+template <typename ValueType = std::string>
+class memory_storage : public storage::memory_storage_data<ValueType>,
+                       public data_storage_acessor<ValueType, memory_storage<ValueType>> {
+public:
+    using value_type         = ValueType;
+    using super              = storage::memory_storage_data<ValueType>;
+
+public:
+    memory_storage(DelayQueue* delay_queue) : super(delay_queue) {
+    }
+    ~memory_storage() {
+        super::release_data();
+    }
+    bool persist_data(std::string table_name, std::string key, value_type data, const storage_config& config) {
+        std::scoped_lock<std::mutex> locker(super::data_mutex);
+        auto& table                                    = super::storage[table_name];
+        auto data_iter                                 = table.find(key);
+        storage::memory_storage_item<value_type>& item = data_iter == table.end() ? table[key] : data_iter->second;
+        if (data_iter != table.end()) {
+            if (!config.overwrite) {
+                FWARN("table:{} key:{} has alread existed", table_name, key);
+                return false;
+            }
+            // clear status
+            super::delay_queue->StopDelayTask(item.handle);
+            item.handle = Fundamental::DelayQueue::kInvalidHandle;
+        }
+        item.data = std::move(data);
+        if (config.expired_time_msec > 0) {
+            item.handle = super::delay_queue->AddDelayTask(
+                config.expired_time_msec,
+                [table_name, key, this]() {
+                    super::remove_data(table_name, key);
+                    super::expired_signal_.Emit(table_name, key);
+                },
+                true);
+            super::delay_queue->StartDelayTask(item.handle);
+        }
+        return true;
+    }
+
+    std::tuple<bool, value_type> get_value(std::string table_name, std::string key) const {
+        bool b_sucess = false;
+        value_type ret;
+
+        do {
+            std::scoped_lock<std::mutex> locker(super::data_mutex);
+            auto table_iter = super::storage.find(table_name);
+            if (table_iter == super::storage.end()) break;
+            auto data_iter = table_iter->second.find(key);
+            if (data_iter == table_iter->second.end()) break;
+            ret      = data_iter->second.data;
+            b_sucess = true;
+        } while (0);
+        return { b_sucess, std::move(ret) };
+    }
+};
+
+template <>
+class memory_storage<void> : public storage::memory_storage_data<void>,
+                             public data_storage_acessor<void, memory_storage<void>> {
+public:
+    using value_type         = void;
+    using super              = storage::memory_storage_data<void>;
+    using normal_access_type = storage::memory_storage_data<void>;
+
+public:
+    memory_storage(DelayQueue* delay_queue) : super(delay_queue) {
+    }
+    ~memory_storage() {
+        super::release_data();
+    }
+    bool persist_data(std::string table_name, std::string key, const storage_config& config) {
+        std::scoped_lock<std::mutex> locker(super::data_mutex);
+        auto& table                                    = super::storage[table_name];
+        auto data_iter                                 = table.find(key);
+        storage::memory_storage_item<value_type>& item = data_iter == table.end() ? table[key] : data_iter->second;
+        if (data_iter != table.end()) {
+            if (!config.overwrite) {
+                FWARN("table:{} key:{} has alread existed", table_name, key);
+                return false;
+            }
+            // clear status
+            super::delay_queue->StopDelayTask(item.handle);
+            item.handle = Fundamental::DelayQueue::kInvalidHandle;
+        }
+        if (config.expired_time_msec > 0) {
+            item.handle = super::delay_queue->AddDelayTask(
+                config.expired_time_msec,
+                [table_name, key, this]() {
+                    super::remove_data(table_name, key);
+                    expired_signal_.Emit(table_name, key);
+                },
+                true);
+            super::delay_queue->StartDelayTask(item.handle);
+        }
+        return true;
+    }
 };
 } // namespace Fundamental
