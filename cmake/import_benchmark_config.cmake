3ecc57a4f65a72fcb3bc2e642d32df918a83978c
lightning <469953258@qq.com>
2025-05-28 13:59:09 +0800

    apply local gtest benchmark

--- cmake/import_benchmark_config.cmake
+++ cmake/import_benchmark_config.cmake
@@ -1,31 +1,50 @@
 if(__import_benchmark_config__)
-    return()
+  return()
 endif()
-set(__import_benchmark_config__ TRUE )
+set(__import_benchmark_config__ TRUE)
 
 
 option(IMPORT_BENCHMARK "import benchmark" ON)
 option(BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library." OFF)
 if(IMPORT_BENCHMARK)
-  include(FetchContent)
-  FetchContent_Declare(
-    benchmark
-    GIT_REPOSITORY http://192.168.50.101:8787/rdd/benchmark.git
-    GIT_TAG f4f93b5553ced834b2120048f65690cddb4b7a2f
-  )
-  option(BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library." OFF)
-  option(BENCHMARK_ENABLE_GTEST_TESTS "Enable building the unit tests which depend on gtest" OFF)
-  option(BENCHMARK_USE_BUNDLED_GTEST "Use bundled GoogleTest. If disabled, the find_package(GTest) will be used." OFF)
-  option(BENCHMARK_ENABLE_INSTALL "Enable installation of benchmark. (Projects embedding benchmark may want to turn this OFF.)" OFF)
-  option(BENCHMARK_ENABLE_DOXYGEN "Build documentation with Doxygen." OFF)
-  option(BENCHMARK_INSTALL_DOCS "Enable installation of documentation." OFF)
-  FetchContent_MakeAvailable(benchmark)
+  find_package(benchmark QUIET)
+  if(benchmark_FOUND)
+    message(STATUS "use system benchmark")
+  else()
+    include(FetchContent)
+    FetchContent_Declare(
+      benchmark
+      GIT_REPOSITORY http://192.168.50.101:8787/rdd/benchmark.git
+      GIT_TAG f4f93b5553ced834b2120048f65690cddb4b7a2f
+    )
+    option(BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library." OFF)
+    option(BENCHMARK_ENABLE_GTEST_TESTS "Enable building the unit tests which depend on gtest" OFF)
+    option(BENCHMARK_USE_BUNDLED_GTEST "Use bundled GoogleTest. If disabled, the find_package(GTest) will be used." OFF)
+    option(BENCHMARK_ENABLE_INSTALL "Enable installation of benchmark. (Projects embedding benchmark may want to turn this OFF.)" OFF)
+    option(BENCHMARK_ENABLE_DOXYGEN "Build documentation with Doxygen." OFF)
+    option(BENCHMARK_INSTALL_DOCS "Enable installation of documentation." OFF)
+    FetchContent_MakeAvailable(benchmark)
+  endif()
+  add_library(import_benchmark_interface INTERFACE)
+  if(TARGET benchmark::benchmark)
+    target_link_libraries(import_benchmark_interface
+      INTERFACE
+      benchmark::benchmark)
+  elseif(TARGET benchmark)
+    target_link_libraries(import_benchmark_interface
+      INTERFACE
+      benchmark)
+  else()
+    message(FATAL_ERROR "Found benchmark but no valid target")
+  endif()
+
 
   function(declare_benchmark_exe target_name)
-
     target_link_libraries(${target_name}
       PRIVATE
-      benchmark::benchmark ${CMAKE_THREAD_LIBS_INIT})
+      import_benchmark_interface)
+
+
     if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "NVHPC")
       target_compile_options(${target_name} PRIVATE --diag_suppress partial_override)
     endif()
