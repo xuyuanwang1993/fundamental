f266230857475edef7a3877f6f721d08b429dd21
lightning <469953258@qq.com>
2025-07-29 11:06:07 +0800

    update submodule

--- cmake/import_gtest_config.cmake
+++ cmake/import_gtest_config.cmake
@@ -15,7 +15,7 @@ if(IMPORT_GTEST)
     include(FetchContent)
     FetchContent_Declare(
       googletest
-      GIT_REPOSITORY http://192.168.50.101:8787/rdd/googletest.git
+      GIT_REPOSITORY git@github.com:xuyuanwang1993/googletest.git
       GIT_TAG 58d77fa8070e8cec2dc1ed015d66b454c8d78850 # release-1.12.1
     )
     set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
